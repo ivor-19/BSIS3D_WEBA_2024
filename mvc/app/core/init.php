c8b26672077635967d758f107b2e8c20a5062242
ivor <ivorcruz19@gmail.com>
2024-03-22 12:06:05 +0800

    latest bro

--- mvc/app/core/init.php
+++ mvc/app/core/init.php
@@ -2,3 +2,13 @@
 
 require 'functions.php';
 require 'App.php';
+require 'Controller.php';
+require 'Database.php';
+require 'App.php';
+
+spl_autoload_register(function ($class_name) {
+
+    require '../app/models/' .  $class_name . '.php';
+});
+
+?>
