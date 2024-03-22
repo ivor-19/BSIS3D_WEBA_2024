fb36fb196c304e00d3661df7f42ab63b6d9e5ae4
ivor <ivorcruz19@gmail.com>
2024-03-22 10:53:52 +0800

    latest

---
+++ mvc/app/core/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class Database{
+    private function connect(){
+        $string = "mysql:host=localhost;dbname=mvcbsis3d_db";
+        $con = new PDO($string,"root","");
+        return $con;   
+
+    }
+    public function query($query, $data = []){
+        $con = $this->connect();
+        $stm = $con->prepare($query);
+
+        $check = $stm->execute($data);
+
+        if($check){
+            $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+        }
+        return false;   
+    }
+}
