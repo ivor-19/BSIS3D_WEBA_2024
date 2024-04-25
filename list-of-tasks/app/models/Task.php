b3be870f4225305a07f40c4b14364199904bd2cf
AceAbela <ablaseannaced@gmail.com>
2024-04-25 21:59:10 +0800

    UPDATED

---
+++ list-of-tasks/app/models/Task.php
@@ -0,0 +1,9 @@
+<?php
+
+class Task extends Model
+{
+
+  public function validate() {
+    //firstname, lastname, email, password
+  }
+}
--- mvc/public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-<<<<<<< HEAD
-require '../app/core/init.php'; 
-=======
-require '../app/core/init.php';
->>>>>>> origin/warren-branch
-
-$app = new App();
